235ea7a07edccc5ea45c27a9c46673614530b775
Kriti Agarwal <kritiagarwal.16@gmail.com>
2016-10-24 18:29:54 -1200

    Add files via upload

---
+++ NumWithMaxFreq.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int a[500],b[500]={0},i,c,n,max;
+    printf("Enter size of array: ");
+    scanf("%d",&n);
+    printf("Enter elements of array: ");
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+        b[a[i]]++;
+    }
+    max=b[0];
+    for(i=1;i<n;i++){
+        if(max<b[i]){
+            max=b[i];
+            c=i;
+        }
+    }
+    printf("Maximum Frequency is: %d\nNumber with maximum Frequency is: %d",max,c);
+    return 0;
+}
